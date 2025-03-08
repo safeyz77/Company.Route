33407e46a39a987c5eb9f04a61fd1d3a6fb4e8c0
MostafaSafey <58831144+safeyz77@users.noreply.github.com>
2025-03-08 06:49:05 +0200

    Impelementatin of DAL

---
+++ DAL/Data/CompanyDbContext/CompanyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data.CompanyDbContext
+{
+    internal class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() :base()
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=. ;Database = CompanyRoute; Trusted_Connection = True; TurstServerCertificate = True;");
+        }
+        public DbSet<Department> Departments { get; set; } 
+    }
+}
---
+++ DAL/Data/Configuration/DepartmentsConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Data.Configuration
+{
+    internal class DepartmentsConfig : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+        }
+    }
+}
