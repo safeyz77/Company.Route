406fc5ef20a59ec27ad4472985f586457aaca78d
MostafaSafey <58831144+safeyz77@users.noreply.github.com>
2025-03-08 07:20:20 +0200

    Department Module BLL

---
+++ BLL/Repos/DepartmentRepo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Interfaces;
+using DAL.Data.CompanyDbContext;
+using DAL.Models;
+
+namespace BLL.Repos
+{
+    public class DepartmentRepo : IDepratmentRepo
+    {
+        private readonly CompanyDbContext _companyDbContext;
+        public DepartmentRepo()
+        {
+            _companyDbContext = new CompanyDbContext();
+        }
+        public IEnumerable<Department> GetAll()
+        {
+            return _companyDbContext.Departments.ToList();
+        }
+
+        public Department? Get(int id)
+        {
+            return _companyDbContext.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+            _companyDbContext.Departments.Add(model);
+            return _companyDbContext.SaveChanges();
+
+        }
+
+        public int Update(Department model)
+        {
+            _companyDbContext.Departments.Update(model);
+            return _companyDbContext.SaveChanges();
+        }
+
+        public int Delete(Department model)
+        {
+            _companyDbContext.Departments.Remove(model);
+            return _companyDbContext.SaveChanges();
+        }
+       
+    }
+}
