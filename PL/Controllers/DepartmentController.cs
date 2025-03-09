000b02a49e69cad2df58c1c49f91da6c07336c97
MostafaSafey <58831144+safeyz77@users.noreply.github.com>
2025-03-09 10:30:28 +0200

    view department

---
+++ PL/Controllers/DepartmentController.cs
@@ -0,0 +1,16 @@
+﻿using BLL.Repos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            DepartmentRepo departmentRepo = new DepartmentRepo();
+            var dep = departmentRepo.GetAll();
+
+            return View(dep);
+        }
+    }
+}
